e0efe77045fa1426f95eedd1a78cd085ef003849
thehairy <soeren.stabenow@outlook.com>
2025-05-05 21:36:56 +0200

    Initial Commit

---
+++ iRPC/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iRPC
+//
+//  Created by Sören Stabenow on 05.05.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iRPC/iRPCApp.swift
@@ -0,0 +1,17 @@
+//
+//  iRPCApp.swift
+//  iRPC
+//
+//  Created by Sören Stabenow on 05.05.25.
+//
+
+import SwiftUI
+
+@main
+struct iRPCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
